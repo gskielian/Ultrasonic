da7ebccb59f40bc19c22b6f2f957a89d69301f8c
gskielian <gregory.kielian@hackerdojo.com>
2013-08-26 16:34:03 -0700

    Create Ultrasonic.ino

---
+++ Ultrasonic.ino
@@ -0,0 +1,41 @@
+\int trigPin = 2;
+int echoPin = 4;
+
+long cm;
+long duration;
+long distance;
+
+void setup()
+{
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);   
+
+ Serial.begin(9600);
+}
+
+
+void loop()
+{
+ 
+  cm = getDistance(); 
+  Serial.println(cm);
+    
+}
+
+long getDistance()
+{
+  delay(10);  
+  //trigger the device
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);  
+
+//how long it has been since we sent the ping  
+  duration = pulseIn(echoPin, HIGH);
+
+//convert the duration to centimeters ( div by 2 because to and frro)
+  cm = duration/27/2;
+  return cm;
+}
